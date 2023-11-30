3e9237e67607768da3d0e5c2c0cf3f1e54ed2be1
TechieGold <techiegold101@gmail.com>
2023-11-30 20:23:50 +0100

    add script that installs nginx on the server

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Manifest to kill a process named "killmenow"
 
-exec { 'killmenow_process':
-  command     => '/usr/bin/pkill -f killmenow',
-  refreshonly => true,
+exec { 'pkill':
+  command     => 'pkill killmenow',
+  provider    => 'shell',
 }
