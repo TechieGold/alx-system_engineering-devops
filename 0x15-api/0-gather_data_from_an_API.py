3a677405db1a53d7cecaa5c0bfe38d01b349794f
TechieGold <techiegold101@gmail.com>
2024-01-03 07:01:43 +0100

    add script that returns info for a given employee id

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    todos = requests.get("{}todos".format(url), params={
+                         "userId": user_id}).json()
+
+    completed = [t["title"] for t in todos if t["completed"]]
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+        user["name"], len(completed), len(todos)))
+    [print("\t{}".format(c)) for c in completed]
