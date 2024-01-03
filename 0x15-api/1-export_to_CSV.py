f4a3fa23e14f22bfeccd2cf1b0189a221da06ec3
TechieGold <techiegold101@gmail.com>
2024-01-03 08:01:14 +0100

    add script that exports todo-list of a specified employee Id to .csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This script exports todo-list information for a given employee id
+into a csv file
+"""
+import requests
+import sys
+import csv
+
+if __name__ == '__main__':
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get("{}users/{}".format(api_url, user_id)).json()
+    todos = requests.get("{}todos".format(api_url),
+                         params={"userId": user_id}).json()
+    username = user.get("username")
+
+    # File name must be: USER_ID.csv
+    csv_filename = "{}.csv".format(user_id)
+    with open(csv_filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+        for todo in todos:
+            row = [user_id, username, todo.get('completed'), todo.get('title')]
+            writer.writerow(row)
