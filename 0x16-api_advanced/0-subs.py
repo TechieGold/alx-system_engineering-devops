299315374c65ec67406a25567f13cdcc494a0e76
TechieGold <techiegold101@gmail.com>
2024-01-17 19:54:02 +0100

    add script that returns the an integer from an api url

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This script querys a reddit api and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'http://www.reddit.com/r/{}/about.json'
+    header = {'User-Agent': 'custom'}
+
+    response = requests.get(url.format(subreddit),
+                            header, allow_redirects=False)
+    if response.status_code == 200:
+        subscribers = response.json().get("data").get("subscribers")
+        return subscribers
+    else:
+        return 0
