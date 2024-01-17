6c842cd57f0b6758d0037d19264f9078ceb7c495
TechieGold <techiegold101@gmail.com>
2024-01-17 20:35:20 +0100

    i print None intead of returning None

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,11 +11,11 @@ def top_ten(subreddit):
     prints the titles of the first 10 hot posts listed for
     a given subreddit, or None if invalid.
     """
-    response = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
+    api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+    params = {"limit": 10}
+
+    response = requests.get(api_url, headers=headers, params=params)
 
     if response.status_code == 200:
         for get_data in response.json().get("data").get("children"):
@@ -23,4 +23,4 @@ def top_ten(subreddit):
             t = d.get("title")
             print(t)
     else:
-        return None
+        print("None")
